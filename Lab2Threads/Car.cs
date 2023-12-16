95c6e6add2d5c1ebf2f90ed7576c06976b9baea9
Balos87 <145317796+Balos87@users.noreply.github.com>
2023-12-16 10:03:17 +0100

    Add project files.

---
+++ Lab2Threads/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace Lab2Threads
+{
+    internal class Car
+    {
+        public string Name { get; set; }
+
+        public Car(string name)
+        {
+            Name = name;
+            Runner();
+        }
+
+        public async Task Runner()
+        {
+            await Task.Delay(5000);
+        }
+    }
+}
---
+++ Lab2Threads/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Concurrent;
+
+namespace Lab2Threads
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            await Console.Out.WriteLineAsync("Hello World!");
+
+            Car car = new Car("Volvo");
+
+            Task.Run(() => { car; });
+        }
+    }
+}
