493e717978b803f55deaed4eac4e32e715a98cea
Balos87 <145317796+Balos87@users.noreply.github.com>
2023-12-28 07:56:30 +0100

    Update
    Fixed the issue. Lots of confusion resolved.

--- Lab2Threads/Car.cs
+++ Lab2Threads/Car.cs
@@ -10,16 +10,96 @@ namespace Lab2Threads
     internal class Car
     {
         public string Name { get; set; }
+        public int DrivingSpeed = 34; // 120km/h - I wanted it converted to meters so this is aprox 34m/s
+        public int SpeedAtStart = 0; // Before GO.
+        public int TrackLenght = 1000; // 10km
+        public int TrackStartPosition = 0; // The Start-line
+        public bool HasFinished = false;
+
+        public int OutOfGasDelayer = 10000;
+
+        private bool IsDelayed = false;
 
         public Car(string name)
         {
             Name = name;
-            Runner();
         }
 
-        public async Task Runner()
-        {
-            await Task.Delay(5000);
+        public async Task StartRaceAsync()
+        {//Method that runs the race actual race.
+
+            var roadProblemsTask = RoadProblemsAsync();
+            await Console.Out.WriteLineAsync($"{Name} just started the race!");
+
+            while (TrackStartPosition < TrackLenght)
+            {
+                if (!IsDelayed)
+                {
+                    TrackStartPosition += DrivingSpeed;
+                }
+
+                await Task.Delay(1000);
+
+                if (TrackStartPosition >= TrackLenght)
+                {
+                    await Console.Out.WriteLineAsync($"{Name} is the winner!");
+                    HasFinished = true;
+                    break;
+                }
+            }
+            await roadProblemsTask;
+
+        }
+
+        public async Task RoadProblemsAsync()
+        {//Method to simulate problems along the race.
+
+            Random whatProblemWillAccure = new Random();
+
+            while ( TrackStartPosition < TrackLenght )
+            {
+                await Task.Delay(5000);
+
+                int randomAccident = whatProblemWillAccure.Next(1, 51);
+
+                if (randomAccident <= 1)
+                {
+                    IsDelayed = true;
+                    await Console.Out.WriteLineAsync($"{Name} is out of gas, need refill fuel.");
+                    await Console.Out.WriteLineAsync($"{Name} is delayed with 20secs");
+                    await Task.Delay(20000);
+                    await Console.Out.WriteLineAsync("Delay has passed.");
+                    IsDelayed = false;
+                }
+                else if (randomAccident <= 3)
+                {
+                    IsDelayed = true;
+                    await Console.Out.WriteLineAsync($"{Name} got a flat tire, need to change it.");
+                    await Console.Out.WriteLineAsync($"{Name} is delayed with 10secs");
+                    await Task.Delay(10000);
+                    await Console.Out.WriteLineAsync("Delay has passed.");
+                    IsDelayed = false;
+                }
+                else if (randomAccident <= 8)
+                {
+                    IsDelayed = true;
+                    await Console.Out.WriteLineAsync($"{Name} got a bird on the windshield, what are the odds... Need to clean it up.");
+                    await Console.Out.WriteLineAsync($"{Name} is delayed with 5secs");
+                    await Task.Delay(5000);
+                    await Console.Out.WriteLineAsync("Delay has passed.");
+                    IsDelayed = false;
+                }
+                else if (randomAccident <= 18)
+                {
+                    await Console.Out.WriteLineAsync($"{Name} got a engine malfunction, speed is reduced.");
+                    DrivingSpeed--;
+                }
+                else
+                {
+                    await Console.Out.WriteLineAsync("Phew, no accident happened, race keeps on going.");
+                }
+            }
+
         }
     }
 }
--- Lab2Threads/Program.cs
+++ Lab2Threads/Program.cs
@@ -6,13 +6,36 @@ namespace Lab2Threads
     {
         static async Task Main(string[] args)
         {
-            CancellationTokenSource cts = new CancellationTokenSource();
+            await Console.Out.WriteLineAsync("Lets race!");
 
-            await Console.Out.WriteLineAsync("Hello World!");
+            Car car1 = new Car("Volvo");
+            Car car2 = new Car("Kia");
 
-            Car car = new Car("Volvo");
+            Task car1Race = Task.Run(() => car1.StartRaceAsync());
+            Task car2Race = Task.Run(() => car2.StartRaceAsync());
 
-            Task.Run(() => { car; });
+            Task displayRace = DisplayCarPositionsAsync(car1, car2);
+
+            await Task.WhenAny(car1Race, car2Race);
+            await Console.Out.WriteLineAsync("Race is over!!");
+        }
+
+        static async Task DisplayCarPositionsAsync(Car car1, Car car2)
+        {
+            while (!car1.HasFinished && !car2.HasFinished) 
+            {
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+
+                    if (key.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine($"{car1.Name} has traveled: {car1.TrackStartPosition} meters");
+                        Console.WriteLine($"{car2.Name} has traveled: {car2.TrackStartPosition} meters");
+                    }
+                }await Task.Delay(100);
+
+            }
         }
     }
 }
